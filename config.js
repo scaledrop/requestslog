56674529fdea53580548358acd51df770d3a1119
Akshaya Raghuvanshi <scaledrop@gmail.com>
2018-04-19 16:10:28 +0530

    client side file moved to public directory

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-exports.appPort = 1300;
+exports.appPort = 8080;
 exports.serverTimeout = 20000;
 exports.database = {
 	path: 'mongodb://localhost:27017/', // local server
-	name: 'eturllog',
+	name: 'urllog',
 	collection: 'urls'
 }
